ca854e153f56e24e4a0831df34b3331d0ed42df6
Alec_Gracelli <alecgracelli@gmail.com>
2023-06-01 17:13:46 -0300

    Controller lanche criada

---
+++ Curso_MVC/Controllers/LancheController.cs
@@ -0,0 +1,22 @@
+﻿using Curso_MVC.Repositories;
+using Curso_MVC.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso_MVC.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
