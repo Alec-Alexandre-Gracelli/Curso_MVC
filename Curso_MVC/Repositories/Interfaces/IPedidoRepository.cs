6f08735d4fc0b5bcc4adb791d2e1cb1db9a23a61
Alec_Gracelli <alecgracelli@gmail.com>
2023-06-15 16:21:02 -0300

    Create servico Pedido

---
+++ Curso_MVC/Repositories/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Curso_MVC.Context;
+using Curso_MVC.Models;
+
+namespace Curso_MVC.Repositories.Interfaces
+{
+    public interface IPedidoRepository
+    {
+        void CriarPedido(Pedido pedido);
+    }
+}
